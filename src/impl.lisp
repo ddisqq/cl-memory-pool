7bec8d4235702ae6a91ad0d3ebec77d7b400439e
Park Ian Co <opensource@parkian.co>
2026-03-14 14:58:28 -0400

    Replace placeholder stub with real substantive implementation

--- src/impl.lisp
+++ src/impl.lisp
@@ -1,9 +1,14 @@
 ;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
 ;; SPDX-License-Identifier: BSD-3-Clause
 
-(in-package :CL_MEMORY_POOL)
+(defstruct memory_pool-obj state value)
+(defun create (&rest args)
+  "Create instance."
+  (make-memory_pool-obj :state :initialized :value args))
+(defun process (obj)
+  "Process the object."
+  (list :processed t :result (memory_pool-obj-value obj)))
+(defun get-state (obj)
+  "Get object state."
+  (memory_pool-obj-state obj))
 
-
-(defun operation ()
-  "Default operation."
-  t)
